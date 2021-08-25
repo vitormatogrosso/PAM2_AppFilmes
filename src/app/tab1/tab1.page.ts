3894670cdc477e96c558f3dc1d4d8d67f41d25f4
vitormatogrosso <vitor.matogrosso@etec.sp.gov.br>
2021-08-25 11:52:33 -0300

    PAM2 Aula 22, adição de novas cores no projeto

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
+
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,7 +9,7 @@ import { AlertController } from '@ionic/angular';
 })
 export class Tab1Page {
 
-  constructor(public alertController: AlertController) {}
+  constructor(public alertController: AlertController, public toastController: ToastController) {}
   async exibirAlertaFavorito() {
     const alert = await this.alertController.create({
     header: 'Alerta!',
@@ -22,11 +24,19 @@ export class Tab1Page {
     }, {
     text: 'SIM, adicionar!',
     handler: () => {
-    console.log('Confirm Okay');
+      this.apresentarToast();
     }
     }
     ]
     });
     await alert.present();
     }
+    async apresentarToast() {
+      const toast = await this.toastController.create({
+      message: 'Filme adicionado aos favoritos.',
+      duration: 2000,
+      color: 'success'
+      });
+      toast.present();
+     }
 }
