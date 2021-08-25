7ca4dc2dbc04e907689d8b6a1dea9a99f56fed4f
vitormatogrosso <vitor.matogrosso@etec.sp.gov.br>
2021-08-24 22:03:28 -0300

    PAM2 A18

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
@@ -7,6 +7,26 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
-
+  constructor(public alertController: AlertController) {}
+  async exibirAlertaFavorito() {
+    const alert = await this.alertController.create({
+    header: 'Alerta!',
+    message: 'Deseja realmente adicionar este filme aos <strong>favoritos</strong>?',
+    buttons: [
+    {
+    text: 'Cancelar',
+    role: 'cancel',
+    handler: (blah) => {
+    console.log('Confirm Cancel: blah');
+    }
+    }, {
+    text: 'Okay',
+    handler: () => {
+    console.log('Confirm Okay');
+    }
+    }
+    ]
+    });
+    await alert.present();
+    }
 }
